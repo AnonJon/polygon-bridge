19dc6d2c89ddd82dcefd9de8ad7b5efb53faab0b
jongregis <jon.gregis@gmail.com>
2022-12-26 17:22:52 -0600

    modules

--- scripts/ERC20.s.sol
+++ scripts/ERC20.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "lib/forge-std/src/Script.sol";
+import "forge-std/Script.sol";
 import "../contracts/ERC20.sol";
 
 contract ERC20script is Script {
